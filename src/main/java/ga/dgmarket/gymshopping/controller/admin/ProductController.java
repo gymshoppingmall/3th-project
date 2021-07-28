c0c6b86998def8bb8039966879aa2b637755021e
Einvlir <seokmin9296@gmail.com>
2021-07-29 00:27:40 +0900

    Admin AOP적용 (로그인 기능) 목록게시판 페이징 처리 (회원게시판)

--- src/main/java/ga/dgmarket/gymshopping/controller/admin/ProductController.java
+++ src/main/java/ga/dgmarket/gymshopping/controller/admin/ProductController.java
@@ -2,6 +2,9 @@ package ga.dgmarket.gymshopping.controller.admin;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +18,7 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	@GetMapping("/main/productlist")
-	public String memberList(Model model) {
+	public String memberList(Model model, HttpServletRequest request) {
 		List productList = productService.selectAll();
 		model.addAttribute("productList",productList);
 		return "admin/main/productlist";
