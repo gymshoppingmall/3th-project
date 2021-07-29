ae04e440907edb7296baf630df1db6980436066f
dlfjs0472 <dlfjs0472@gmail.com>
2021-07-30 01:34:00 +0900

    상세보기 준비

---
+++ src/main/java/ga/dgmarket/gymshopping/controller/member/ProductController.java
@@ -0,0 +1,18 @@
+package ga.dgmarket.gymshopping.controller.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+	
+	// 물품 상세보기 요청처리 --도균--
+		@RequestMapping(value = "/detail", method = RequestMethod.GET)
+		public String detailform() {
+			
+			return "member/shop/detail";
+		}
+	
+	
+}
