9e5fb8809d95daa02f2dd436b7dcf0f1943f4d70
sungil3441 <whtjddlf56@naver.com>
2021-07-31 00:39:12 +0900

    ..

--- src/main/java/ga/dgmarket/gymshopping/controller/member/ProductController.java
+++ src/main/java/ga/dgmarket/gymshopping/controller/member/ProductController.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ProductController {
 	
-	// 물품 상세보기 요청처리 --도균--
-		@RequestMapping(value = "/detailform", method = RequestMethod.GET)
-		public String detailform() {
-			
-			return "member/shop/detail";
-		}
-	
-	
+// 물품 상세보기 요청처리 --도균--
+	@RequestMapping(value = "/detailform", method = RequestMethod.GET)
+	public String detailform() {
+		
+		return "member/shop/detail";
+	}
 }
