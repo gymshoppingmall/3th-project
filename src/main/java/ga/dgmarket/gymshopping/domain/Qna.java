7f3d2ad4b42c32de62c219c139c913daafee4410
sungil3441 <whtjddlf56@naver.com>
2021-08-02 03:50:19 +0900

    qna vo, mapper 생성[성일]

---
+++ src/main/java/ga/dgmarket/gymshopping/domain/Qna.java
@@ -0,0 +1,15 @@
+package ga.dgmarket.gymshopping.domain;
+
+import lombok.Data;
+
+@Data
+public class Qna {
+	private int qna_id;
+	private int member_id;
+	private String title;
+	private String content;
+	private String regdate;
+	private int team;
+	private int step;
+	private int depth;
+}
