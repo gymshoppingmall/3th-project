5d7e5b81de36a4cd8d22eb48a92aefe49888d6e9
sungil3441 <whtjddlf56@naver.com>
2021-08-02 17:34:28 +0900

    Used service 수정[성일]

--- src/main/java/ga/dgmarket/gymshopping/model/service/usedproduct/UsedProductServiceImpl.java
+++ src/main/java/ga/dgmarket/gymshopping/model/service/usedproduct/UsedProductServiceImpl.java
@@ -86,18 +86,6 @@ public class UsedProductServiceImpl implements UsedProductService{
 	//세션에 있는 member_id을 통해 조회할 예정
 	public List selectAll(HttpServletRequest request) {
 		HttpSession session = request.getSession();
-		
-		//테스트하려고 정보 넣는 중==============
-		Member member = new Member();
-		member.setMember_id(6);
-		member.setStorename("성일샵");
-		member.setName("성일");
-		member.setUser_id("used_test");
-		member.setPassword("1234");
-		
-		session.setAttribute("member", member);
-		//==========================
-		
 		Member mem = (Member)session.getAttribute("member");
 		return usedProductDAO.selectAll(mem.getMember_id());
 	}	
